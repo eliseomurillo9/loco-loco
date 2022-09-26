f2d260571669ad7039b6fbf663bd300e29276684
Caroline <liardcar@gmail.com>
2022-09-26 16:45:02 +0200

    feat: config base integration files

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,13 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('', name: 'main_')]
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig');
     }
 }
