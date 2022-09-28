2926de5beb5e7de5263bd4c5fe8a0744b5bae71e
Caroline <liardcar@gmail.com>
2022-09-28 13:12:35 +0200

    chore: link register path navbar

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,13 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/user', name: 'user_')]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('', name: 'register')]
+    public function register(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        return $this->render('user/register.html.twig');
     }
 }
