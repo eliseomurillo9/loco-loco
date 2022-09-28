fe2ec74a7818aee84ccc3d17530d9ef16a743675
Caroline <liardcar@gmail.com>
2022-09-28 14:18:21 +0200

    chore: link paths navbar

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,12 +6,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/user', name: 'user_')]
+#[Route('', name: 'user_')]
 class UserController extends AbstractController
 {
-    #[Route('', name: 'register')]
+    #[Route('/register', name: 'register')]
     public function register(): Response
     {
         return $this->render('user/register.html.twig');
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('user/login.html.twig');
+    }
+
+    #[Route('/favorite', name: 'favorite')]
+    public function favorite(): Response
+    {
+        return $this->render('user/favorite.html.twig');
+    }
+
+    #[Route('/grocery-list', name: 'grocery-list')]
+    public function groceryList(): Response
+    {
+        return $this->render('user/grocery-list.html.twig');
+    }
+
+    #[Route('/profil-client', name: 'profil-client')]
+    public function profilClient(): Response
+    {
+        return $this->render('user/profil-client.html.twig');
+    }
+
+    #[Route('/profil-pro', name: 'profil-pro')]
+    public function profilPro(): Response
+    {
+        return $this->render('user/profil-pro.html.twig');
+    }
+
+    #[Route('/add-place', name: 'add-place')]
+    public function addPlace(): Response
+    {
+        return $this->render('user/add-place.html.twig');
     }
 }
