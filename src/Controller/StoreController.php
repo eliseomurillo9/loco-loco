b23f3a3ad9b663ad0714611d428349b2247c6e1d
Caroline <liardcar@gmail.com>
2022-10-03 12:41:04 +0200

    chore: pro profil

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -9,19 +9,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StoreController extends AbstractController
 {
-    #[Route('/store', name: 'app_store')]
-    public function index(): Response
-    {
-        return $this->render('store/index.html.twig', [
-            'controller_name' => 'StoreController',
-        ]);
-    }
-
     #[Route('/store/locator', name: 'store_locator')]
     public function storeLocator(): Response
     {
-        return $this->render('store/farms-locator.html.twig', [
-            'controller_name' => 'StoreController',
-        ]);
+        return $this->render('store/farms-locator.html.twig');
+    }
+
+    #[Route('/store/single', name: 'store_single')]
+    public function storeSingle(): Response
+    {
+        return $this->render('store/single.html.twig');
+    }
+
+    #[Route('/store', name: 'store_index')]
+    public function storeIndex(): Response
+    {
+        return $this->render('store/index.html.twig');
     }
 }
